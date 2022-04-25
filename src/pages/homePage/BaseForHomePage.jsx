7de606492e1c128bce3d37a22c7fd34aa79118bf
Nishad Lad <ladnishad@gmail.com>
2022-04-25 16:58:31 -0500

    Trial api.

--- src/pages/homePage/BaseForHomePage.jsx
+++ src/pages/homePage/BaseForHomePage.jsx
@@ -4,17 +4,41 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { CenteredTextComponent } from "../components/CenteredTextComponent"
 
+const getAircraftsNearUser = async({ long, lat }) => {
+    const reqBody = {
+      startLongitude: long,
+      startLatitude: lat,
+    }
+
+    const headers = {
+      "Authorization": "", //Add jwt here
+    }
+    const airports = await axios.get("localhost:5000/airports/nearby", { data: reqBody}, { headers: headers })
+
+    console.log(airports)
+}
+
 export const BaseForHomePage = () => {
+  const [position, setPosition] = useState({ lat: 0, long: 0})
     useEffect(() => {
       if("geolocation" in  navigator){
         navigator.geolocation.getCurrentPosition(async function(position) {
-            console.log(`Location is ${position.coords.latitude}, ${position.coords.longitude}`)
+          setPosition({
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          })
         })
       }
       else{
         console.log("Not available")
       }
-    })
+    }, [])
+
+    useEffect(() => {
+      getAircraftsNearUser({ long: position.long, lat: position.lat })
+    }, [position])
+
+    console.log(position)
     return (
       <CenteredTextComponent text="Home" />
     )
