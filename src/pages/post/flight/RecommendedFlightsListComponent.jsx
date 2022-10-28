169456f51cf84033c66ebcda568a031c0417f2f1
Nishad Lad <ladnishad@gmail.com>
2022-10-28 16:56:00 -0500

    Fixed card width in grid

--- src/pages/post/flight/RecommendedFlightsListComponent.jsx
+++ src/pages/post/flight/RecommendedFlightsListComponent.jsx
@@ -5,7 +5,7 @@ import { RecommendedFlightCard } from "./RecommendedFlightCardComponentNew"
 
 export const RecommendedFlightsList = memo(({ setOpenConfirmAddFlightDialog, flights, selectedFlight, setSelectedFlight }) => {
     return flights.map(( flight, i ) => (
-      <Grid item xs={12} md={flights?.length > 1 ? 6 : 12} sx={{ pt:1 }}>
+      <Grid item xs={12} sx={{ pt:1 }}>
         <RecommendedFlightCard identifier={i} setOpenConfirmAddFlightDialog={setOpenConfirmAddFlightDialog} flight={flight} selectedFlight={selectedFlight} setSelectedFlight={setSelectedFlight} />
       </Grid>
     ))
