43439ac16923943646395c5f1f88955203c459dc
Nishad Lad <ladnishad@gmail.com>
2023-01-19 14:13:34 -0600

    Fixed notifications subscription

--- src/pages/notifications/NotificationsListComponent.jsx
+++ src/pages/notifications/NotificationsListComponent.jsx
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import dayjs from "dayjs"
-import relativeTime from 'dayjs/plugin/relativeTime'
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListDivider from '@mui/joy/ListDivider';
-import ListItemContent from '@mui/joy/ListItemContent';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import ListItemButton from '@mui/joy/ListItemButton';
-import Avatar from '@mui/joy/Avatar';
-import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
-import Typography from '@mui/joy/Typography';
+import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItemContent from "@mui/joy/ListItemContent";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
+import ListItemButton from "@mui/joy/ListItemButton";
+import Avatar from "@mui/joy/Avatar";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+import Typography from "@mui/joy/Typography";
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
 export const NotificationsList = ({ userId, notifications }) => {
   return (
     <List>
-      {
-        notifications.map(({ actorUserId, action, entity, timestamp }) => {
-          const notification = `${actorUserId === userId ? "You": "Your friend"} ${action} a ${entity}`
-          const timeSinceNotification = dayjs(timestamp).fromNow()
+      {notifications.map(({ actorUserId, action, entity, timestamp }) => {
+        const notification = `${
+          actorUserId === userId ? "You" : "Your friend"
+        } ${action} a ${entity}`;
+        const timeSinceNotification = dayjs(timestamp).fromNow();
 
-          return (
-            <>
-              <ListItem variant='soft' color='primary'>
-                <ListItemDecorator sx={{ alignSelf: 'flex-start' }}>
-                  <Avatar />
-                </ListItemDecorator>
-                <ListItemContent>
-                  <Typography level="body1">{notification}</Typography>
-                  <Typography level="body4" noWrap>
-                    {timeSinceNotification}
-                  </Typography>
-                </ListItemContent>
-              </ListItem>
-              <ListDivider inset="gutter" />
-            </>
-          )
-        })
-      }
+        return (
+          <>
+            <ListItem variant="soft" color="primary">
+              <ListItemDecorator
+                sx={{ alignSelf: "flex-start", marginRight: "20px" }}
+              >
+                <Avatar />
+              </ListItemDecorator>
+              <ListItemContent>
+                <Typography level="body1" align="left">
+                  {notification}
+                </Typography>
+                <Typography level="body4" noWrap align="left">
+                  {timeSinceNotification}
+                </Typography>
+              </ListItemContent>
+            </ListItem>
+            <ListDivider inset="gutter" />
+          </>
+        );
+      })}
     </List>
-  )
-}
+  );
+};
